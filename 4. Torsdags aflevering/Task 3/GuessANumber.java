fd98b419fd4623b5bb03c3b18bcacc4bc7ca50a6
RodneyMuyanga <144030309+RodneyMuyanga@users.noreply.github.com>
2023-09-28 14:09:21 +0200

    4. Torsdags aflevering

--- 4. Torsdags aflevering/Task 3/GuessANumber.java
+++ 4. Torsdags aflevering/Task 3/GuessANumber.java
@@ -1,44 +1,44 @@
 import java.util.Random;
-import java.util.Scanner;  
+import java.util.Scanner;
 
+public class GuessANumber {
 
-public class GuessANumber 
-{
-	
     private static int rnd_number;
 
     public static void main(String[] args) {
-        // pick a random number
+        // Pick a random number
         Random random = new Random();
         rnd_number = random.nextInt(100) + 1;
-        System.out.println( "I'm thinking of a number between 1 and 100 (including both).");
-		System.out.println( "Can you guess what it is?...");
+        System.out.println("I'm thinking of a number between 1 and 100 (including both).");
+        System.out.println("Can you guess what it is?...");
         makeAGuess();
- 	}
-
-
-    public static void makeAGuess()
-    {
-		Scanner scanner = new Scanner(System.in); // Create a Scanner object.
-
-    	String input = scanner.nextLine(); // Read user input.
-    	
-        //use hasNextDouble to check if input is numeric.
-        if (scanner.hasNextDouble())
-        {
-            double number = scanner.nextDouble();
-            System.out.println("You have entered a double: " + number);
-        }    
-        if (number == rnd_number)
-        {
-            System.out.println("Congratulations!");
-        }      
-        // if so...
-        //   Compare it with the random number
-        //   Let the user know the result of the comparison
-        //   Let the user try again by calling this method recursively
-        //   Help the user by revealing wether the guess was lower or higher than the target number
-        // if input was not numeric show an error message and call this method recursively
     }
 
+    public static void makeAGuess() {
+        Scanner scanner = new Scanner(System.in); // Create a Scanner object.
+
+        // Read user input as a string
+        String input = scanner.nextLine();
+
+        // Check if the input is numeric
+        if (scanner.hasNextDouble()) {
+            double number = scanner.nextDouble();
+            
+            // Compare the user's guess with the random number
+            if (number == rnd_number) {
+                System.out.println("Congratulations! You guessed it right.");
+            } else if (number < rnd_number) {
+                System.out.println("Try again. Your guess is too low.");
+                makeAGuess(); // Call this method recursively for another guess
+            } else {
+                System.out.println("Try again. Your guess is too high.");
+                makeAGuess(); // Call this method recursively for another guess
+            }
+        } else {
+            System.out.println("You did not enter a valid number. Please try again.");
+            makeAGuess(); // Call this method recursively for another guess
+        }
+        
+        scanner.close();
+    }
 }
