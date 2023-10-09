3cc5eed8a771c81c7246dbe1a040505cd35cb59a
RodneyMuyanga <144030309+RodneyMuyanga@users.noreply.github.com>
2023-10-09 19:10:32 +0200

    5. torsdagsopgave

--- 1. semester/5. Torsdags aflevering/IntelliJ/Task 1 - ArrayList og objekter/src/Task1/Customer.java
+++ 1. semester/5. Torsdags aflevering/IntelliJ/Task 1 - ArrayList og objekter/src/Task1/Customer.java
@@ -1,4 +1,63 @@
-public class Customer
+package Task1;
+
+public class Customer //Entity Class
 {
+    //Her har jeg 4 attributter/felter/instansvariabler.
+    private String firstName;
+    /*private attributer (encapsulation). Dette gør jeg for at instansvariablerne ikke er tilgængelige før jeg henter dem manuelt efter ønske.
+    På denne måde blander felterne sig ikke med øvrige klasser. OGSÅ KALDET (DATAHIDE)*/
+    private String lastName;
+    private String username;
+    private int id;
+
+public Customer(String firstName, String lastName, String username, int id) //konstruktør bruges til at initialisere og oprette et nyt objekt. Nødvendigt for at bygge flere forskellige kunder.
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.id = id;
+
+    }
+
+    public String toString() //toString repræsentere et læstbart String af mit Objekt. Hvis ikke denne metode er i brug så laver compileren et "@" efterfulgt af noget tilfældigt.
+    {
+        return "Firstname: " + firstName + " Lastname: " + lastName + " Username: " + username + " Your Id: " + id;
+    }
+
+    //Setters:
+    public void setFirstName(String newfirstName)
+    {
+        firstName = newfirstName;
+    }
+    public void setlastName(String newlastName)
+    {
+        lastName = newlastName;
+    }
+    public void setusername(String newusername)
+    {
+        username = newusername;
+    }
+    public void setid(int newid)
+    {
+        id = newid;
+    }
+
+    //Getters:
+    public int getid()
+    {
+        return id;
+    }
+    public String getfirstName()
+    {
+        return firstName;
+    }
+    public String getlastName()
+    {
+        return lastName;
+    }
+    public String getusername()
+    {
+        return username;
+    }
 
 }
--- 1. semester/5. Torsdags aflevering/IntelliJ/Task 1 - ArrayList og objekter/src/Task1/Main.java
+++ 1. semester/5. Torsdags aflevering/IntelliJ/Task 1 - ArrayList og objekter/src/Task1/Main.java
@@ -1,6 +1,38 @@
-public class Main {
-    public static void main(String[] args)
-    {
+package Task1;
 
+import java.util.ArrayList;
+
+public class Main { //client Class
+
+    public static void main(String[] args) //Jeg har brugt en main metode, som man skal have for at køre nogle af sine koder.
+    {
+        ArrayList<Customer> Customers = new ArrayList<>(); // <> bruges til at bestemme hvilken klasse Arraylisten skal gemme.
+
+        Customer customer1 = new Customer("Jens", "Mortensen", "JM93", 55);
+        Customer customer2 = new Customer("Henna", "Upoi", "HU55", 810);
+        Customer customer3 = new Customer("Micheal", "Petersen", "MMPP19", 65);
+        Customer customer4 = new Customer("Peter", "Nymann", "Willy9000", 75);
+        Customer customer5 = new Customer("Patrick", "Thomsen", "JeffTheCheff", 445);
+        Customer customer6 = new Customer("Nanna", "Camillasen", "CamGirl22", 956);
+        Customer customer7 = new Customer("Henriette", "Pedersen", "Hot123", 95);
+        Customer customer8 = new Customer("Amanda", "Nielsen", "AmNi", 751);
+
+        Customers.add(customer1);
+        Customers.add(customer2);
+        Customers.add(customer3);
+        Customers.add(customer4);
+        Customers.add(customer5);
+        Customers.add(customer6);
+        Customers.add(customer7);
+        Customers.add(customer8);
+
+        printCustomers(Customers);
+    }
+    public static void printCustomers(ArrayList<Customer> customers)
+    {
+        for (Customer customer : customers)
+        {
+            System.out.println(customer);
+        }
     }
 }
