caa8a6244ec19bd94877f8aca213a9b1655a145b
RodneyMuyanga <144030309+RodneyMuyanga@users.noreply.github.com>
2023-10-27 12:15:58 +0200

    WhatsUp

--- Github øvelse/githubDemo/src/main/java/org/example/Main.java
+++ Github øvelse/githubDemo/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class Main {
+    public static int i = 10;
+    public static int j = 10;
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("WhatsUp!");
     }
 }
