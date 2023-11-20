7c5642670aef7cf3ccaed468341460639f07a9cb
RodneyMuyanga <144030309+RodneyMuyanga@users.noreply.github.com>
2023-11-20 10:50:07 +0100

    d

--- SP 2 - Implementation/SP2-Implementation/src/main/java/org/example/FileIO.java
+++ SP 2 - Implementation/SP2-Implementation/src/main/java/org/example/FileIO.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class FileIO {
 
     public ArrayList<String> readCarData(String path) {
